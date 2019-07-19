f07db3fcffd2a248d9d77ad711b0c477eab90aaa
sunny setia <sunnysetia93@gmail.com>
2019-07-20 01:57:20 +0530

    actionCreators redux

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,2 +1,17 @@
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const REMOVE_INGREDIENT = 'REMOVE_INGREDIENT';
+
+
+export const add = (ingName) =>{
+    return {
+        type:ADD_INGREDIENT,
+        ingredientName:ingName
+    }
+}
+
+export const remove = (ingName) =>{
+    return {
+        type:REMOVE_INGREDIENT,
+        ingredientName:ingName
+    }
+}
